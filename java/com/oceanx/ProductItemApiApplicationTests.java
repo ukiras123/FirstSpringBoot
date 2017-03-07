56410d3e80e548a8a27e20031e5d86724fdd9b7b
Kiran Gautam <kgautam@oceanx.com>
2017-03-06 22:03:53 -0800

    added unit tests

---
+++ java/com/oceanx/ProductItemApiApplicationTests.java
@@ -0,0 +1,18 @@
+package com.oceanx;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@TestPropertySource(locations = "classpath:application-local.properties")
+public class ProductItemApiApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
---
+++ src/test/java/com/kiran/controller/SurveyControllerIT.java
@@ -0,0 +1,84 @@
+package com.kiran.controller;
+
+import static org.junit.Assert.assertTrue;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.json.JSONException;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.skyscreamer.jsonassert.JSONAssert;
+import org.springframework.boot.context.embedded.LocalServerPort;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.kiran.Application;
+import com.kiran.model.Question;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = Application.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class SurveyControllerIT {
+
+	@LocalServerPort
+	private int port;
+
+	TestRestTemplate restTemplate = new TestRestTemplate();
+
+	HttpHeaders headers = new HttpHeaders();
+
+	@Before
+	public void before() {
+
+		headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
+
+	}
+
+	@Test
+	public void testRetrieveSurveyQuestion() throws JSONException {
+
+		HttpEntity<String> entity = new HttpEntity<String>(null, headers);
+
+		ResponseEntity<String> response = restTemplate.exchange(
+				createURLWithPort("/surveys/Survey1/questions/Question1"), HttpMethod.GET, entity, String.class);
+
+		String expected = "{id:Question1,description:Largest Country in the World,correctAnswer:Russia}";
+
+		JSONAssert.assertEquals(expected, response.getBody(), false);
+	}
+
+	@Test
+	public void retrieveAllSurveyQuestions() throws Exception {
+		ResponseEntity<List<Question>> response = restTemplate.exchange(createURLWithPort("/surveys/Survey1/questions"),
+				HttpMethod.GET, new HttpEntity<String>("DUMMY_DOESNT_MATTER", headers),
+				new ParameterizedTypeReference<List<Question>>() {
+				});
+		Question sampleQuestion = new Question("Question1", "Largest Country in the World", "Russia",
+				Arrays.asList("India", "Russia", "United States", "China"));
+		assertTrue(response.getBody().contains(sampleQuestion));
+	}
+
+	@Test
+	public void addQuestion() {
+		Question question = new Question("DOESNTMATTER", "Question1", "Russia",
+				Arrays.asList("India", "Russia", "United States", "China"));
+		HttpEntity entity = new HttpEntity<Question>(question, headers);
+		ResponseEntity<String> response = restTemplate.exchange(createURLWithPort("/surveys/Survey1/questions"),
+				HttpMethod.POST, entity, String.class);
+		String actual = response.getHeaders().get(HttpHeaders.LOCATION).get(0);
+		assertTrue(actual.contains("/surveys/Survey1/questions/"));
+	}
+
+	private String createURLWithPort(final String uri) {
+		return "http://localhost:" + port + uri;
+	}
+
+}
